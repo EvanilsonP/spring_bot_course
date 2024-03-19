076124a25e72491a3ec5a54b83a909177abbf6f1
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-19 16:00:20 -0300

    Injecting Custom Application Properties

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // injecting properties for coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose a new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach name " + coachName + ", Team name " + teamName;
+    }
+
     // expose "/" that returns Hello World!
     @GetMapping("/")
     public String helloWorld() {
