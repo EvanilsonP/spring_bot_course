fc9994aff585fad78678a27ef116444170b6257d
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-19 14:57:45 -0300

    Dev tools - coding

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -10,4 +10,14 @@ public class FunRestController {
     public String helloWorld() {
         return "Hello World!";
     }
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getFortune() {
+        return "Today is your lucky day!";
+    }
 }
